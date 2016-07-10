eb7cb3258c9911ca5acb6adc33c39d007d8e8961
Jordan Ryan Reuter <me@jreut.com>
2016-07-09 21:39:01 -0400

    numpad, media controls, brightness for Mac

--- keyboards/planck/keymaps/jreut/keymap.c
+++ keyboards/planck/keymaps/jreut/keymap.c
@@ -47,10 +47,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 },
 
 [_RAISE] = {
-  {_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______},
-  {KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12},
-  {_______, KC_DEL, KC_CUT, KC_COPY, KC_PASTE, KC_MENU, KC_CAPS, KC_INSERT, KC_PSCREEN, KC_SLCK, KC_PAUSE, _______},
-  {_______,_______,_______,_______,_______,_______,_______,_______,KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY}
+  {KC_MUTE,KC_VOLD,KC_VOLU,KC_BTN1,KC_MS_U,KC_BTN2,_______,_______,KC_P7,KC_P8,KC_P9,KC_PAST},
+  {KC_MRWD,KC_MPLY,KC_MFFD,KC_MS_L,KC_MS_D,KC_MS_R,_______,_______,KC_P4,KC_P5,KC_P6,KC_PSLS},
+  {KC_SLCK,KC_PAUSE,_______,_______,_______,_______,_______,_______,KC_P1,KC_P2,KC_P3,KC_PMNS},
+  {_______,_______,_______,_______,_______,_______,_______,_______,KC_P0,KC_PDOT,KC_PENT,KC_PPLS}
 },
 
 [_PLOVER] = {
