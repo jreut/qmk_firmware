61cfd5160a87999d53ba6a64b74f4b4941234734
Jordan Ryan Reuter <me@jreut.com>
2016-07-09 20:50:46 -0400

    reformat

--- keyboards/planck/keymaps/jreut/keymap.c
+++ keyboards/planck/keymaps/jreut/keymap.c
@@ -13,7 +13,6 @@ extern keymap_config_t keymap_config;
 #define _LOWER 3
 #define _RAISE 4
 #define _PLOVER 5
-#define _NOTES 6
 #define _ADJUST 16
 
 enum planck_keycodes {
@@ -21,7 +20,6 @@ enum planck_keycodes {
   COLEMAK,
   DVORAK,
   PLOVER,
-  NOTES,
   LOWER,
   RAISE,
   BACKLIT,
@@ -35,39 +33,31 @@ enum planck_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = {
-  {KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC},
-  {KC_BSLS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT},
-  {KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT},
-  {KC_LCTL, KC_LGUI, KC_LALT, KC_TAB, LOWER, KC_SPC, KC_SPC, RAISE, KC_RALT, KC_RGUI, KC_RCTL, KC_ENT}
+  {KC_ESC ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_BSPC},
+  {KC_BSLS,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,KC_QUOT},
+  {KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_ENT },
+  {KC_LCTL,KC_LGUI,KC_LALT,KC_TAB ,LOWER  ,KC_SPC ,KC_SPC ,RAISE  ,KC_RALT,KC_RGUI,KC_RCTL,KC_TAB }
 },
+
 [_LOWER] = {
-  {KC_MINS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_EQL},
-  {KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,KC_TRNS,KC_TRNS},
-  {KC_TRNS, KC_TRNS, KC_TRNS, KC_GRV, KC_TILD, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_TRNS},
-  {KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_HOME, KC_PGDOWN, KC_PGUP, KC_END}
+  {KC_MINS,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_EQL },
+  {_______,_______,_______,_______,_______,_______,KC_LEFT,KC_DOWN,KC_UP  ,KC_RIGHT,_______,_______},
+  {_______,_______,_______,KC_GRV ,KC_TILD,_______,_______,KC_LBRC,KC_RBRC,KC_LCBR,KC_RCBR,_______},
+  {_______,_______,_______,_______,_______,_______,_______,_______,KC_HOME,KC_PGDOWN,KC_PGUP,KC_END}
 },
 
 [_RAISE] = {
-  {KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS},
+  {_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______},
   {KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12},
-  {KC_TRNS, KC_DEL, KC_CUT, KC_COPY, KC_PASTE, KC_MENU, KC_CAPS, KC_INSERT, KC_PSCREEN, KC_SLCK, KC_PAUSE, KC_TRNS},
-  {KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY}
+  {_______, KC_DEL, KC_CUT, KC_COPY, KC_PASTE, KC_MENU, KC_CAPS, KC_INSERT, KC_PSCREEN, KC_SLCK, KC_PAUSE, _______},
+  {_______,_______,_______,_______,_______,_______,_______,_______,KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY}
 },
-
-
 
 [_PLOVER] = {
   {KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1   },
   {XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC},
   {XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {EXT_PLV, XXXXXXX, XXXXXXX, KC_C,    KC_V,    XXXXXXX, XXXXXXX, KC_N,    KC_M,    XXXXXXX, XXXXXXX, XXXXXXX}
-},
-
-[_NOTES] = {
-  {KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS},
-  {KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS},
-  {KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS},
-  {KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS}
 },
 
 /* Adjust (Lower + Raise)
@@ -83,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = {
   {_______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL},
-  {_______, _______, _______, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, DVORAK,  PLOVER,  NOTES},
+  {_______, _______, _______, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, DVORAK,  PLOVER, _______},
   {_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 }
@@ -136,12 +126,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           PLAY_NOTE_ARRAY(tone_dvorak, false, 0);
         #endif
         persistent_default_layer_set(1UL<<_DVORAK);
-      }
-      return false;
-      break;
-    case NOTES:
-      if (record->event.pressed) {
-        persistent_default_layer_set(1UL<<_NOTES);
       }
       return false;
       break;
