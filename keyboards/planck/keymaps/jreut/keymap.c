ef743350de2088bc7d77d73f8596a250ca6dc586
Jordan Ryan Reuter <me@jreut.com>
2016-08-21 13:05:11 -0400

    Put backslash on to a mod/tap key

--- keyboards/planck/keymaps/jreut/keymap.c
+++ keyboards/planck/keymaps/jreut/keymap.c
@@ -30,17 +30,14 @@ enum planck_keycodes {
 enum {
   OPE = 0,
   CLS,
-  SLS,
 };
 
 //Tap Dance Definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
-  //Tap once for right shift, twice for enter
-  /* [TD_RSFT_ENT]  = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_ENT) */
   // opening bracket & brace
   [OPE] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_LCBR),
-  [CLS] = ACTION_TAP_DANCE_DOUBLE(KC_RBRC, KC_RCBR),
-  [SLS] = ACTION_TAP_DANCE_DOUBLE(KC_SLSH, KC_BSLS)
+  // closing bracket & brace
+  [CLS] = ACTION_TAP_DANCE_DOUBLE(KC_RBRC, KC_RCBR)
 };
 
 // Fillers to make layering more clear
@@ -51,8 +48,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = {
   {KC_GRV ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_BSPC},
   {ALL_T(KC_TAB),KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,KC_QUOT},
-  {KC_LSPO,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,KC_N   ,KC_M   ,KC_COMM,KC_DOT ,TD(SLS),KC_RSPC},
-  {KC_LCTL,KC_LGUI,KC_LALT,TD(OPE),LOWER  ,KC_ESC ,KC_SPC ,RAISE  ,TD(CLS),KC_RALT,KC_RGUI,CTL_T(KC_ENT)}
+  {KC_LSPO,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSPC},
+  {KC_LCTL,KC_LGUI,KC_LALT,TD(OPE),LOWER  ,KC_ESC ,KC_SPC ,RAISE  ,TD(CLS),KC_RALT,GUI_T(KC_BSLS),CTL_T(KC_ENT)}
 },
 
 [_LOWER] = {
