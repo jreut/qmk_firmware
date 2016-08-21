693695c461fed047608724e5b83ef898112c087d
Jordan Ryan Reuter <me@jreut.com>
2016-08-21 12:16:15 -0400

    Experiment with tap dance

--- keyboards/planck/keymaps/jreut/keymap.c
+++ keyboards/planck/keymaps/jreut/keymap.c
@@ -26,6 +26,17 @@ enum planck_keycodes {
   EXT_PLV
 };
 
+//Tap Dance Declarations
+enum {
+  TD_RSFT_ENT = 0
+};
+
+//Tap Dance Definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+  //Tap once for right shift, twice for enter
+  [TD_RSFT_ENT]  = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_ENT)
+};
+
 // Fillers to make layering more clear
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
@@ -36,13 +47,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_ESC ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_BSPC},
   {KC_TAB ,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,KC_QUOT},
   {KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLS},
-  {KC_LCTL,KC_LGUI,KC_LALT,XXXXXXX,LOWER  ,KC_SPC ,KC_SPC ,RAISE  ,KC_RALT,KC_RGUI,KC_RSFT,KC_ENT }
+  {KC_LCTL,KC_LGUI,KC_LALT,XXXXXXX,LOWER  ,KC_SPC ,KC_SPC ,RAISE  ,KC_RALT,KC_RGUI,TD(TD_RSFT_ENT),KC_ENT }
 },
 
 [_LOWER] = {
   {KC_MINS,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_EQL },
   {_______,XXXXXXX,KC_END ,KC_PGUP,KC_PGDOWN,KC_HOME,KC_LEFT,KC_DOWN,KC_UP  ,KC_RIGHT,XXXXXXX,XXXXXXX},
-  {_______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,KC_GRV ,KC_TILD,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX},
+  {_______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,KC_GRV ,KC_TILD,XXXXXXX,XXXXXXX,_______,XXXXXXX,XXXXXXX},
   {_______,_______,_______,_______,_______,KC_LBRC,KC_RBRC,_______,_______,_______,_______,_______}
 },
 
@@ -62,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
- * |      | Reset|      |      |      |      |      |      |      |      |      |  Del |
+ * | Power| Reset|      |      |      |      |      |      |      |      |      |  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |      |      |Aud on|Audoff|AGnorm|AGswap|Qwerty|Colemk|Dvorak|Plover|      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
