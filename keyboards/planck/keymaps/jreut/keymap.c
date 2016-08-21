30f5cd778d9617319ac77bd03d83b7cbbdcc1737
Jordan Ryan Reuter <me@jreut.com>
2016-08-21 12:29:12 -0400

    Space Cadet shift and overloaded enter

--- keyboards/planck/keymaps/jreut/keymap.c
+++ keyboards/planck/keymaps/jreut/keymap.c
@@ -28,13 +28,17 @@ enum planck_keycodes {
 
 //Tap Dance Declarations
 enum {
-  TD_RSFT_ENT = 0
+  OPE = 0,
+  CLS,
 };
 
 //Tap Dance Definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
   //Tap once for right shift, twice for enter
-  [TD_RSFT_ENT]  = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_ENT)
+  /* [TD_RSFT_ENT]  = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_ENT) */
+  // opening bracket & brace
+  [OPE] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_LCBR),
+  [CLS] = ACTION_TAP_DANCE_DOUBLE(KC_RBRC, KC_RCBR),
 };
 
 // Fillers to make layering more clear
@@ -42,26 +46,26 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 #define XXXXXXX KC_NO
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
+// missing: KC_BSLS
 [_QWERTY] = {
   {KC_ESC ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_BSPC},
   {KC_TAB ,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,KC_QUOT},
-  {KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLS},
-  {KC_LCTL,KC_LGUI,KC_LALT,XXXXXXX,LOWER  ,KC_SPC ,KC_SPC ,RAISE  ,KC_RALT,KC_RGUI,TD(TD_RSFT_ENT),KC_ENT }
+  {KC_LSPO,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSPC},
+  {KC_LCTL,KC_LGUI,KC_LALT,TD(OPE),LOWER  ,KC_SPC ,KC_SPC ,RAISE  ,TD(CLS),KC_RALT,KC_RGUI,CTL_T(KC_ENT)}
 },
 
 [_LOWER] = {
   {KC_MINS,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_EQL },
   {_______,XXXXXXX,KC_END ,KC_PGUP,KC_PGDOWN,KC_HOME,KC_LEFT,KC_DOWN,KC_UP  ,KC_RIGHT,XXXXXXX,XXXXXXX},
   {_______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,KC_GRV ,KC_TILD,XXXXXXX,XXXXXXX,_______,XXXXXXX,XXXXXXX},
-  {_______,_______,_______,_______,_______,KC_LBRC,KC_RBRC,_______,_______,_______,_______,_______}
+  {_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______}
 },
 
 [_RAISE] = {
   {KC_MUTE,KC_VOLD,KC_VOLU,KC_BTN1,KC_MS_U,KC_BTN2,XXXXXXX,XXXXXXX,KC_P7,KC_P8,KC_P9,KC_PAST},
   {KC_MRWD,KC_MPLY,KC_MFFD,KC_MS_L,KC_MS_D,KC_MS_R,XXXXXXX,XXXXXXX,KC_P4,KC_P5,KC_P6,KC_PSLS},
   {KC_SLCK,KC_PAUSE,XXXXXXX,XXXXXXX,XXXXXXX,KC_GRV ,KC_TILD,XXXXXXX,KC_P1,KC_P2,KC_P3,KC_PMNS},
-  {_______,_______,_______,_______,_______,KC_LCBR,KC_RCBR,_______,KC_P0,KC_PDOT,KC_PENT,KC_PPLS}
+  {_______,_______,_______,_______,_______,_______,_______,_______,KC_P0,KC_PDOT,KC_PENT,KC_PPLS}
 },
 
 [_PLOVER] = {
